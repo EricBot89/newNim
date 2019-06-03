7e9bfab744d5f15d2671767f372f2f38dd7dfa8a
EricBot89 <eloucks@gmail.com>
2019-06-03 14:21:51 -0400

    front end foundations laid

---
+++ client/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class Main extends React.Component {
+
+  render(){
+    return(
+  <div id="app">
+    <h1> Soon an App will be here!</h1>
+    <p>cool huh?</p>
+  </div>
+  )
+}
+
+}
+
+
+ReactDOM.render( <Main /> ,document.getElementById("root"))
--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
 const express = require("express");
-const morgan = require("morgan");
+const volleyball = require("volleyball");
+const path = require("path");
 const { db } = require("./db");
 const { auth, stats} = require("./api");
 
 const server = express();
 const PORT = 8080;
 
-server.use(express.static("../public"))
+const APP_CORE = path.join(__dirname,"..","public","index.html");
 
-server.use(morgan)
+server.use(volleyball)
+server.use(express.json())
 
 server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+
+
+
+server.use(express.static(path.join(__dirname,"..","public")))
 
 server.use("/auth", auth);
 server.use("/stats", stats);
+
+server.get("/", (req, res, next) => {
+  res.status(200).sendFile(APP_CORE)
+})
 
 
 function init (){
