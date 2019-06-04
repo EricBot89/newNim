3326b6e2ba1d83b20fee06ad8d44ab1280cf259c
EricBot89 <eloucks@gmail.com>
2019-06-03 20:10:11 -0400

    added wp configs to the git repo

---
+++ prod.config.js
@@ -0,0 +1,25 @@
+module.exports =  {
+    "mode": "production",
+    "entry": "./client/index.js",
+    "output": {
+        "path": __dirname+'/public',
+        "filename": "[name].[chunkhash:8].js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports =  {
+    "mode": "development",
+    "entry": "./client/index.js",
+    "output": {
+        "path": __dirname+'/public',
+        "filename": "bundle.js"
+    },
+    "devtool": "source-map",
+    "module": {
+        "rules": [
+            {
+                "enforce": "pre",
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": "eslint-loader"
+            },
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
